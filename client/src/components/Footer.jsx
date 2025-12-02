fc148cad56fe95c2b247f78ebd5221056c83c956
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-12-02 13:48:36 +0800

    header footer components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,14 @@
 function Footer() {
   return (
-    <div className="static bottom-0 w-full h-12  flex justify-center items-center mt-8 border-t border-black">
+    <div className="absolute text-white bottom-0 w-full h-12  flex justify-center items-center gap-2">
       &copy; 2025 Redjeacs
+      <a href="https://github.com/redjeacs/hidden-object" target="_blank">
+        <img
+          src="../src/assets/github-mark-white.svg"
+          alt="GitHub"
+          className="w-5"
+        />
+      </a>
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="flex justify-between p-4">
-      <div className="text-2xl font-bold text-white">Hidden Object Game</div>
-      <div className="flex">
+    <div className="flex justify-between items-center p-4">
+      <div className="text-2xl font-bold text-white">Hidden Object</div>
+      <div className="flex gap-4 text-l">
         <Link to="/" className="text-white">
           Home
         </Link>
