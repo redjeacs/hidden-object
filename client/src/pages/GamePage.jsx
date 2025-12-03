2559092e196ea5618b792fbe9240738bf32012f9
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-12-03 14:01:16 +0800

    setCoords on image click
    using getBoundingClientRect to return the
    position and size of image then locate the coords
    relative to the image with event coords

--- client/src/pages/GamePage.jsx
+++ client/src/pages/GamePage.jsx
@@ -1,14 +1,53 @@
-import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function GamePage() {
+  const { gameId } = useParams();
+  const [game, setGame] = useState({
+    id: "abar4akdj;ld",
+    title: "Studio Ghibli: Spirited Away",
+    img: "../src/assets/ghibli.jpg",
+  });
+  const [coords, setCoords] = useState({ x: null, y: null });
+
+  // useEffect(() => {
+  //   async function fetchGame() {
+  //     try {
+  //       const res = fetch(`https://localhost:8080/game/${gameId}`);
+  //       const data = await res.json();
+
+  //       if (res.ok) {
+  //         setGame(data);
+  //       } else {
+  //         console.error("error fetching game");
+  //       }
+  //     } catch (err) {
+  //       console.error("fetch unsuccessful");
+  //     }
+  //   }
+  //   fetchGame();
+  // }, [gameId]);
+
+  function handleImageClick(e) {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setCoords({ x: x, y: y });
+  }
+
   return (
     <>
-      <Header />
+      <div className="flex text-white justify-center gap-8">
+        <div>Timer</div>
+        <div>Objects</div>
+      </div>
       <div className="w-screen h-100% overflow-auto">
         <img
-          src="../src/assets/ghibli.jpg"
+          src={game.img}
           alt=""
           className="w-auto h-auto max-w-none max-h-none object-contain"
           style={{ minWidth: "100%", minHeight: "100%" }}
+          onClick={handleImageClick}
         />
       </div>
     </>
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,5 +1,4 @@
 import Card from "../components/Card";
-import Header from "../components/Header";
 
 const games = [
   {
