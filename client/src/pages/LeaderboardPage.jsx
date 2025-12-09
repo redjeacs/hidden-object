6ad4e82684e45ef093a34c9a63cadd163e62dc38
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-12-09 15:46:14 +0800

    fetch and display leaderboard logic

--- client/src/pages/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 function LeaderboardPage() {
   const { games } = useOutletContext();
-  console.log(games);
+  const [gameId, setGameId] = useState(games[0].id);
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    const fetchScores = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/game/${gameId}/leaderboard`
+        );
+        if (!res.ok) console.error("failed to fetch leaderboard");
+        const scores = res.json();
+        setLeaderboard(scores);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchScores();
+  }, [gameId]);
 
   return (
     <div className="flex flex-col h-full items-center justify-center">
@@ -10,13 +29,23 @@ function LeaderboardPage() {
       <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] min-w-[300px] max-w-[90vw] p-4 text-white justify-items-center items-center self-center gap-8">
         {games.map((game) => {
           return (
-            <div className="max-w-[320px] max-h-[340px] rounded-xl bg-gray-800">
-              <img src={game.img} alt="" />
-              <div>{game.title}</div>
+            <div
+              onClick={() => setGameId(game.id)}
+              className="max-w-[320px] max-h-[340px] rounded-xl bg-gray-800"
+            >
+              <img
+                src={game.img}
+                alt=""
+                className="w-[320px] max-h-[240px] object-cover rounded-t-xl"
+              />
+              <div className="flex flex-col p-4 items-center gap-2">
+                {game.title}
+              </div>
             </div>
           );
         })}
       </div>
+      <div>leaderboard</div>
     </div>
   );
 }
